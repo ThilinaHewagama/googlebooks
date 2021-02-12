57c09ef4857cb24042b2359eff98726b98c99967
Thilina Hewagama <thilina3001@gmail.com>
2021-02-12 17:37:05 +0530

    UITableViewDelegate / DataSource implementation

--- GoogleBooks/Application/ViewControllers/BookFinderViewController.swift
+++ GoogleBooks/Application/ViewControllers/BookFinderViewController.swift
@@ -17,5 +17,33 @@ class BookFinderViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+}
+
+//MARK: UITableViewDataSource
+
+extension BookFinderViewController: UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+        let cell = tableView.dequeueReusableCell(withIdentifier: "BookCell", for: indexPath) as! BookCell
+        cell.configure()
+        return cell
+
+    }
 
 }
+
+//MARK: UITableViewDelegate
+
+extension BookFinderViewController: UITableViewDelegate {
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+
+    }
+
+}
+
