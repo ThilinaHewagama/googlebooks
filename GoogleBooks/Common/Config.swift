6e9b7681ae298c7991bad3495ccfb4d3b083d53b
Thilina Hewagama <thilina3001@gmail.com>
2021-02-12 17:16:47 +0530

    Project Structure

---
+++ GoogleBooks/Common/Config.swift
@@ -0,0 +1,12 @@
+//
+//  Config.swift
+//  GoogleBooks
+//
+//  Created by Thilina Hewagama on 2021-02-12.
+//
+
+import Foundation
+
+struct Config{
+
+}
---
+++ GoogleBooks/Common/Helpers/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  GoogleBooks
+//
+//  Created by Thilina Hewagama on 2021-02-12.
+//
+
+import Foundation
+
+struct Constants{
+    
+}
